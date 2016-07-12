7a56a7b4b10280c9caef072224b7d247c6e3c53d
bastengao <bastengao@gmail.com>
2016-07-13 00:30:00 +0800

    First blood

---
+++ README.md
@@ -0,0 +1,22 @@
+# scp-sync
+
+Auto sync local files to remote through ssh when files change.
+
+### Usage
+
+```
+npm install scp-sync -g
+
+scp-sync -h 192.168.0.1 -p 22 -u vagrant -p vagrant source /home/vagrant/
+
+# source
+#  |- test.txt
+#
+#
+# /home/vagrant/source
+#                |- test.txt
+```
+
+### License
+
+The package is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,32 @@
+var chokidar = require('chokidar');
+var client = require('scp2');
+var extend = require('util')._extend;
+
+var source = 'test';
+var target = '/home/vagrant/'
+
+var ssh = {
+  host: '127.0.0.1',
+  port: 2222,
+  username: 'vagrant',
+  password: 'vagrant',
+}
+
+function scpSync (source, target, ssh) {
+  chokidar.watch(source).on('all', (event, path) => {
+    console.log(event, path);
+    if(event == 'add' || event == 'change') {
+      var options = extend({}, ssh)
+      options.path = target + path;
+      client.scp(path, options, function(err) {
+        if(err) {
+          console.log(err)
+        } else {
+          console.log('send ' + path + ' to remote')
+        }
+      })
+    }
+  });
+}
+
+module.exports = scpSync;
